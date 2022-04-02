c43a9c97329763cc458969e41f0df0807f16a338
Irving78rus <isakov911@mail.ru>
2022-04-03 00:18:48 +0300

    renamed variables, cleaned up the code

--- src/index.tsx
+++ src/index.tsx
@@ -10,31 +10,15 @@ import { BrowserRouter } from 'react-router-dom';
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
-
 export const store = createStore(rootReducer, applyMiddleware(thunk))
- 
-
-
- 
 
  const rootElement = document.getElementById('root');
  if (!rootElement) throw new Error('Failed to find the root element');
  const root = ReactDOMClient.createRoot(rootElement);
-
-
- 
  
 root.render(
   <BrowserRouter>
 <Provider store={store}>
   <App />
 </Provider>
-</BrowserRouter>
-
-
-,)
-
-// ReactDOM.render(
-    
-//   document.getElementById('root')
-// );
+</BrowserRouter>)
