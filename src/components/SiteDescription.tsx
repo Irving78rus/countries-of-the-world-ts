39236602dd7b23a133743cfa2de349d59c4fad71
Irving78rus <54670767+Irving78rus@users.noreply.github.com>
2022-04-06 21:33:12 +0300

    Update SiteDescription.tsx
    delete class card

--- src/components/SiteDescription.tsx
+++ src/components/SiteDescription.tsx
@@ -2,7 +2,7 @@ import '../App.css';
 
 export const SiteDescription: React.FC = () => {
     return (
-        <div className="Card description" >
+        <div className="description" >
             <h1> На этом сайте вы сможете узнать главную информацию о всех странах мира, а так же сравнить их на вкладке Comparsion</h1>
 
         </div>
