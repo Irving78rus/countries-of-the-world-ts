a56415abf980ccd21671396c1f67d0ed201a8cd5
Irving78rus <isakov911@mail.ru>
2022-04-09 00:32:25 +0300

    add component Earth

--- src/components/SiteDescription.tsx
+++ src/components/SiteDescription.tsx
@@ -2,10 +2,8 @@ import '../App.css';
 import Earth from './Earth';
 
 export const SiteDescription: React.FC = () => {
-
     return (<>
         <Earth />
-
         <div className="description" >
             <h1> На этом сайте вы сможете узнать главную информацию о всех странах мира, а так же сравнить их на вкладке Comparsion</h1>
 
