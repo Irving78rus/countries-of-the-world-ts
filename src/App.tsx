85c11bc1507b843efbb742c02ff8c587e98e5685
Irving78rus <isakov911@mail.ru>
2022-04-10 15:10:44 +0300

    Merge branch 'master' of https://github.com/Irving78rus/Flags-on-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,46 @@
-import React , { useEffect }from 'react';
-import './App.css';
-import { SiteDescription } from './components/SiteDescription';
-import {useAppSelector,useAppDispatch} from './hook'
-import HeaderButtons from './components/HeaderButtons';
-import { Route, Routes } from 'react-router-dom';
-import Country from './components/Country';
-import Comparison from './components/Comparison';
-import { requestCountries } from './redux/action';
-import {useTheme}  from './hook'
-import Earth from './components/Earth';
+import React, { useEffect } from "react";
+import "./App.css";
+import { SiteDescription } from "./components/SiteDescription";
+import { useAppSelector, useAppDispatch } from "./hook";
+import HeaderButtons from "./components/HeaderButtons";
+import { Route, Routes } from "react-router-dom";
+import Country from "./components/Country";
+import Comparison from "./components/Comparison";
+import { requestCountries } from "./redux/action";
+import { useTheme } from "./hook";
+import Earth from "./components/Earth";
 function App() {
- 
-
   const dispatch = useAppDispatch();
-    useEffect(() => {
-      dispatch(requestCountries());
-    }, []);
+  useEffect(() => {
+    dispatch(requestCountries());
+  }, []);
 
   const countryLoaded = useAppSelector(
     (state) => state.counterReducer.countries.length
   );
-// локал сторедж дарк тему футер
+  // локал сторедж дарк тему футер
   return (
     <div className="App">
-      <header className="header" >
+      <header className="header">
         <HeaderButtons />
-         
       </header>
-      {countryLoaded>249?null:<div>"Подгружаются данные о странах, пожалуйста, подождите..."</div>   } 
+      {countryLoaded > 249 ? null : (
+        <div>"Подгружаются данные о странах, пожалуйста, подождите..."</div>
+      )}
+
       <main className="main">
-      
-      
-      <Routes>
-      <Route path="countries-of-the-world-ts" element={<SiteDescription />} />
-        <Route path="navigation" element={<Country />} />
-        <Route path="Comparison" element={<Comparison />} />
-      </Routes>
-      
+        <Routes>
+          <Route
+            path="/"
+            element={<SiteDescription />}
+          />
+          <Route path="navigation" element={<Country />} />
+          <Route path="Comparison" element={<Comparison />} />
+        </Routes>
       </main>
-      
 
       <footer className="footer">
-       <div>Данные о странах</div>
+        <div>Данные о странах</div>
       </footer>
     </div>
   );
